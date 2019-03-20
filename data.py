bccc3df1e10be741aa80d6fd696994f63a1a36c8
wuyang0329 <wuyang0329@163.com>
2019-03-20 20:40:14 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,22 +1,22 @@
 from __future__ import print_function
-from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
 import numpy as np
 import os
-import glob
 import skimage.io as io
 import skimage.transform as trans
 import cv2
 import warnings
+
 warnings.filterwarnings("ignore")
 
+BackGround = [255, 255, 255]
 
-Sky = [255,255,255]
+road = [0, 0, 0]
+COLOR_DICT = np.array([BackGround, road])
 
-other = [0,0,0]
-COLOR_DICT = np.array([Sky,other])
 
 class data_preprocess:
-    def __init__(self, train_path, image_folder, label_folder, test_path, save_path, img_rows=256, img_cols=256):
+    def __init__(self, train_path, image_folder, label_folder, test_path, save_path, img_rows=512, img_cols=512):
         self.img_rows = img_rows
         self.img_cols = img_cols
         self.train_path = train_path
@@ -32,13 +32,20 @@ class data_preprocess:
                                   vertical_flip=True,
                                   horizontal_flip=True,
                                   fill_mode='nearest')
-        self.image_color_mode = "grayscale"
+        self.image_color_mode = "rgb"
         self.label_color_mode = "grayscale"
 
         self.flag_multi_class = False
         self.num_class = 2
         self.target_size = (512, 512)
-        self.img_type = 'tif'
+        self.img_type = 'png'
+
+    def label2class(self, label):
+        x = np.zeros([512, 512, 12])
+        for i in range(512):
+            for j in range(512):
+                x[i, j, int(label[i][j])] = 1  # 属于第m类，第三维m处值为1
+        return x
 
     def adjustData(self, img, label, flag_multi_class, num_class):
         if (flag_multi_class):
@@ -49,13 +56,15 @@ class data_preprocess:
                 new_label[label == i, i] = 1
             new_label = np.reshape(new_label, (new_label.shape[0], new_label.shape[1] * new_label.shape[2],
                                                new_label.shape[3])) if flag_multi_class else np.reshape(new_label, (
-            new_label.shape[0] * new_label.shape[1], new_label.shape[2]))
+                new_label.shape[0] * new_label.shape[1], new_label.shape[2]))
             label = new_label
         elif (np.max(img) > 1):
             img = img / 255.
             label = label / 255.
             label[label > 0.5] = 1
             label[label <= 0.5] = 0
+            # img = img_to_array(img)
+            # label = self.label2class(img_to_array(label))
         return (img, label)
 
     def trainGenerator(self, batch_size, image_save_prefix="image", label_save_prefix="label",
@@ -106,12 +115,21 @@ class data_preprocess:
         img = img[:, :, 0] if len(img.shape) == 3 else img
         img_out = np.zeros(img.shape + (3,))
         for i in range(self.num_class):
+            print('test')
             img_out[img == i, :] = color_dict[i]
-        return img_out / 255.
+        return img_out
 
-    def saveResult(self, npyfile, size, name, threshold=80):
+    def saveResult(self, npyfile, size, name):
         for i, item in enumerate(npyfile):
             img = self.labelVisualize(self.num_class, COLOR_DICT, item) if self.flag_multi_class else item[:, :, 0]
             img = cv2.resize(img, size, interpolation=cv2.INTER_CUBIC)
-            ret, thresh = cv2.threshold(img, threshold / 255.0, 1.0, cv2.THRESH_TOZERO)
-            cv2.imwrite(os.path.join(self.save_path, ("%s_predict." + self.img_type) % (name)), thresh)
+            cv2.imwrite(os.path.join(self.save_path, ("%s_predict." + self.img_type) % (name)),img)
+            img = cv2.imread(os.path.join(self.save_path, ("%s_predict." + self.img_type) % (name)))
+            for k in range(len(img)):
+                for j in range(len(img[k])):
+                    num = img[k][j][0]
+                    if num == 0:
+                        img[k][j] = road
+                    else:
+                        img[k][j] = BackGround
+            cv2.imwrite(os.path.join(self.save_path, ("%s_predict." + self.img_type) % (name)), img)
