2f121852c3be9ce0b9d0249bd9e3b4f2abdf2c5a
wuyang0329 <wuyang0329@163.com>
2019-04-04 20:02:58 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,16 +1,17 @@
 #encoding:utf-8
-# from model import *
-from model_rgb import  *
+from model_v2 import  *
 from data import *
-import numpy as np
-import cv2
 import os
-import skimage.io as io
-import skimage.transform as trans
-import warnings
 import keras
-from keras import Model
-warnings.filterwarnings("ignore")
+from keras.callbacks import TensorBoard
+import tensorflow as tf
+import keras.backend.tensorflow_backend as K
+import matplotlib.pyplot as plt
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth=True
+sess = tf.Session(config=config)
+K.set_session(sess)
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
@@ -18,22 +19,32 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 if __name__ == '__main__':
 
     #path to images which are prepared to train a model
-    train_path = "./data/road/train"
-    image_folder = "images_new"
-    label_folder = "labels_new"
-
-    #path to images which you want to seg
-    test_path = "./road/test/images"
-
-    #save the predict images
-    save_path = "./road/test/predict"
-
-    dp = data_preprocess(train_path,image_folder,label_folder,test_path,save_path)
+    train_path = "data/CamVid"
+    image_folder = "train"
+    label_folder = "trainannot"
+    log_filepath = './log'
+    flag_multi_class = True
+    dp = data_preprocess(train_path=train_path,image_folder=image_folder,label_folder=label_folder)
 
     # train your own model
     train_data = dp.trainGenerator(batch_size=2)
-    model = unet()
+    test_data = dp.testGenerator()
+    model = unet(num_class=12)
 
-    model_checkpoint = keras.callbacks.ModelCheckpoint('./model/road_model_v3.hdf5', monitor='loss',verbose=1,save_best_only=True)
-    model.fit_generator(train_data,steps_per_epoch=300,epochs=20,callbacks=[model_checkpoint])
+    tb_cb = TensorBoard(log_dir=log_filepath)
+    model_checkpoint = keras.callbacks.ModelCheckpoint('./model/road_model_0404_v3.hdf5', monitor='loss',verbose=1,save_best_only=True)
+    history = model.fit_generator(train_data,steps_per_epoch=200,epochs=30,callbacks=[model_checkpoint,tb_cb])
 
+    # draw the loss and accuracy curve
+    plt.figure(12, figsize=(6, 6), dpi=60)
+    plt.subplot(211)
+    plt.plot(history.history['loss'], label='train')
+    plt.title('loss')
+    plt.legend()
+
+    plt.subplot(212)
+    plt.plot(history.history['acc'], label='train')
+    plt.title('acc')
+    plt.legend()
+
+    plt.show()
