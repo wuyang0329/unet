c091fa7eaee1510499cddafc1abf717e1023bb4a
wuyang0329 <wuyang0329@163.com>
2019-04-05 14:45:46 +0800

    Update train.py

--- train.py
+++ train.py
@@ -19,35 +19,45 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 if __name__ == '__main__':
 
     #path to images which are prepared to train a model
-    train_path = "data/CamVid"
+    train_path = "CamVid"
     image_folder = "train"
     label_folder = "trainannot"
+    valid_path =  "CamVid"
+    valid_image_folder ="val"
+    valid_label_folder = "valannot"
     log_filepath = './log'
     flag_multi_class = True
-    num_class = 12
-    dp = data_preprocess(train_path=train_path,
-                         image_folder=image_folder,label_folder=label_folder, 
-                         flag_multi_class = flag_multi_class,
-                         num_classes=num_class)
+    num_classes = 12
+    dp = data_preprocess(train_path=train_path,image_folder=image_folder,label_folder=label_folder,
+                         valid_path=valid_path,valid_image_folder=valid_image_folder,valid_label_folder=valid_label_folder,
+                         flag_multi_class=flag_multi_class,
+                         num_classes=num_classes)
 
     # train your own model
     train_data = dp.trainGenerator(batch_size=2)
+    valid_data = dp.validLoad(batch_size=2)
     test_data = dp.testGenerator()
-    model = unet(num_class=12)
+    model = unet(num_class=num_classes)
 
     tb_cb = TensorBoard(log_dir=log_filepath)
-    model_checkpoint = keras.callbacks.ModelCheckpoint('./model/road_model.hdf5', monitor='loss',verbose=1,save_best_only=True)
-    history = model.fit_generator(train_data,steps_per_epoch=200,epochs=30,callbacks=[model_checkpoint,tb_cb])
+    model_checkpoint = keras.callbacks.ModelCheckpoint('./model/CamVid_model_0405_v1.hdf5', monitor='val_loss',verbose=1,save_best_only=True)
+    history = model.fit_generator(train_data,
+                                  steps_per_epoch=200,epochs=30,
+                                  validation_steps=10,
+                                  validation_data=valid_data,
+                                  callbacks=[model_checkpoint,tb_cb])
 
     # draw the loss and accuracy curve
     plt.figure(12, figsize=(6, 6), dpi=60)
     plt.subplot(211)
     plt.plot(history.history['loss'], label='train')
+    plt.plot(history.history['val_loss'], label='val')
     plt.title('loss')
     plt.legend()
 
     plt.subplot(212)
     plt.plot(history.history['acc'], label='train')
+    plt.plot(history.history['val_acc'], label='val')
     plt.title('acc')
     plt.legend()
 
